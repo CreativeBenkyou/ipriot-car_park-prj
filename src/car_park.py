57630ff76a5dad05d1f372ee2fc564590d7ec2ea
CreativeBenkyou <[J123978@tafe.wa.edu.au]>
2025-06-08 21:01:33 +0800

    feat: add constructor and attributes to car_park.py class

--- src/car_park.py
+++ src/car_park.py
@@ -3,9 +3,18 @@ GLOBAL_ATTRIBUTES = "global_attributes"
 class CarPark():
     GLOBAL_ATTRIBUTES = "global_attributes"
 
-    def __init__(self):
+    def __init__(self, location = "", capacity = "", plates = None, sensors = None, displays = None):
+        '''
+        This function is the initialiser for the CarPark class
+        '''
+        location = self.location
+        capacity = self.capacity
+        plates = self.plates
+        sensors = self.sensors
+        displays = self.displays
 
-        attributes = self.attributes
+    def __str__(self):
+        print(f"This is the {location} Car Park, there are {capacity} bays available.")
 
     def methods(self):
         pass
