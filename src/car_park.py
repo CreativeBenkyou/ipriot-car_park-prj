7ae0715c2f38127692ea7fc879bd2048b4f4ad76
CreativeBenkyou <[J123978@tafe.wa.edu.au]>
2025-06-08 16:46:56 +0800

    feat: add car_park.py, sensors.py, display.py, add class structure to each file

---
+++ src/car_park.py
@@ -0,0 +1,12 @@
+GLOBAL_ATTRIBUTES = "global_attributes"
+
+class CarPark():
+    GLOBAL_ATTRIBUTES = "global_attributes"
+
+    def __init__(self):
+
+        attributes = self.attributes
+
+    def methods(self):
+        pass
+
---
+++ src/display.py
@@ -0,0 +1,12 @@
+GLOBAL_ATTRIBUTES = "global_attributes"
+
+class Display():
+    GLOBAL_ATTRIBUTES = "global_attributes"
+
+    def __init__(self):
+
+        attributes = self.attributes
+
+    def methods(self):
+        pass
+
